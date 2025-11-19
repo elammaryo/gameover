79fb5201a7cf1c31938354eeaab866ff5e033069
elammaryo <oa.elammary@gmail.com>
2025-11-19 02:06:13 -0500

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,44 +1,34 @@
 import logo from '../../public/gameover-logo.png'
 
 export function NavBar({ selectedTab }: { selectedTab?: string }) {
-  const tabStyle = 'text-white hover:underline'
-  const selectedTabStyle = 'text-white font-bold'
+  const tabs = ['studio', 'beats', 'playlists', 'tech', 'about']
+
   return (
-    <nav className='fixed top-0 z-50 flex w-full items-center justify-between bg-[#111111] bg-black/50 py-6 pr-18 pl-10 backdrop-blur-sm'>
+    <nav className='fixed top-0 z-50 flex w-full items-center justify-between gap-8 bg-[#111111] bg-black/50 px-8 py-6 backdrop-blur-sm sm:pr-18 sm:pl-10'>
       <a href='/'>
-        <img src={logo.src} className='h-3.5 w-auto' alt='Logo' />
+        <img
+          src={logo.src}
+          className='h-3.5 w-auto object-contain'
+          alt='Logo'
+        />
       </a>
-      <div className='space-x-12'>
-        <a
-          href='/studio'
-          className={selectedTab === 'studio' ? selectedTabStyle : tabStyle}
-        >
-          Studio
-        </a>
-        <a
-          href='/beats'
-          className={selectedTab === 'beats' ? selectedTabStyle : tabStyle}
-        >
-          Beats
-        </a>
-        <a
-          href='/playlists'
-          className={selectedTab === 'playlists' ? selectedTabStyle : tabStyle}
-        >
-          Playlists
-        </a>
-        <a
-          href='/tech'
-          className={selectedTab === 'tech' ? selectedTabStyle : tabStyle}
-        >
-          Tech
-        </a>
-        <a
-          href='/contact'
-          className={selectedTab === 'contact' ? selectedTabStyle : tabStyle}
-        >
-          Contact
-        </a>
+      <div className='flex space-x-5 sm:space-x-12'>
+        {tabs.map(tab => (
+          <div className='flex flex-col'>
+            <a
+              key={tab}
+              href={`/${tab}`}
+              className={
+                'text-white' + (selectedTab === tab ? ' font-bold' : '')
+              }
+            >
+              {tab.charAt(0).toUpperCase() + tab.slice(1)}
+            </a>
+            {selectedTab === tab && (
+              <div className='mt-1 h-0.5 w-full bg-white'></div>
+            )}
+          </div>
+        ))}
       </div>
     </nav>
   )
