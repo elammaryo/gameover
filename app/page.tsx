66a03dbe871f7c9d078f268e25827d1cbd7e4ad4
elammaryo <oa.elammary@gmail.com>
2025-11-23 03:51:44 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,6 +58,7 @@ export default function Home() {
               className='h-screen object-cover px-12 pt-16'
               alt='Sentinel'
               unoptimized
+              loading='lazy'
             />
           </div>
 
