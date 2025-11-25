fcc25fa9c28edde782341f86262b2307f815b35e
elammaryo <oa.elammary@gmail.com>
2025-11-25 18:05:39 -0500

    Add Spotify access token refresh function

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,12 +1,15 @@
+const credentials = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+const authHeader = `Basic ${Buffer.from(credentials).toString('base64')}`
+const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN || ''
+const tokenEndpoint = 'https://accounts.spotify.com/api/token'
+
 export async function requestToken() {
   try {
-    const res = await fetch('https://accounts.spotify.com/api/token', {
+    const res = await fetch(tokenEndpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
-        ).toString('base64')}`
+        Authorization: authHeader
       },
       body: 'grant_type=client_credentials'
     })
@@ -20,5 +23,34 @@ export async function requestToken() {
   } catch (error) {
     console.error('Error requesting Spotify token:', error)
     throw new Error('Failed to fetch Spotify token')
+  }
+}
+
+export async function refreshAccessToken(): Promise<string | null> {
+  const body = new URLSearchParams({
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken
+  })
+
+  try {
+    const response = await fetch(tokenEndpoint, {
+      method: 'POST',
+      headers: {
+        Authorization: authHeader,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body.toString()
+    })
+
+    if (!response.ok) {
+      throw new Error(`Failed to refresh token: ${response.statusText}`)
+    }
+
+    const data: { access_token: string; [key: string]: any } =
+      await response.json()
+    return data.access_token
+  } catch (error) {
+    console.error('Error during token refresh:', error)
+    return null
   }
 }
