4924ab48bb64925d64516fb594f945002be359a1
elammaryo <oa.elammary@gmail.com>
2025-12-05 17:38:45 -0500

    clean up

--- app/api/beats/route.ts
+++ app/api/beats/route.ts
@@ -1,16 +1,6 @@
 import { NextResponse } from 'next/server'
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import beatsData from './beats.json'
 import { BeatTrack } from '@/app/models/Track'
-
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-  }
-})
 
 export async function GET() {
   try {
